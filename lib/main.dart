44e3f9c5b4322b302184fe8ade616e9ca240611b
David Jo <war2290@gmail.com>
2024-04-17 11:52:16 +0900

    step01 : main_page

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_kakao/pages/main_page.dart';
 import 'package:flutter_kakao/theme.dart';
 
 void main() {
@@ -13,6 +14,7 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       debugShowCheckedModeBanner: true,
       theme: theme(),
+      home: MainPage(),
     );
   }
 }
