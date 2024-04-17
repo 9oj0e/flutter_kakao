1752de80f4ecfe32c1546d0e91c69e48bbeb5a29
David Jo <war2290@gmail.com>
2024-04-17 15:53:37 +0900

    step06 : chat_room_page01

--- lib/pages/chat_room_page.dart
+++ lib/pages/chat_room_page.dart
@@ -1,15 +1,35 @@
 import 'package:flutter/material.dart';
+import '../components/my_chat.dart';
+import 'package:intl/intl.dart';
 
 class ChatRoomPage extends StatefulWidget {
-  const ChatRoomPage({super.key});
-
   @override
   State<ChatRoomPage> createState() => _ChatRoomPageState();
 }
 
 class _ChatRoomPageState extends State<ChatRoomPage> {
+  final List<MyChat> chats = [];
+  final TextEditingController _textController = TextEditingController();
+
   @override
   Widget build(BuildContext context) {
     return Container();
+  }
+
+  void _handleSubmitted(text) {
+    _textController.clear(); // 1
+
+    setState(() {
+      // 2
+      chats.add(
+        MyChat(
+          text: text,
+          time: DateFormat("a K:m") // 3
+              .format(new DateTime.now())
+              .replaceAll("AM", "오전")
+              .replaceAll("PM", "오후"),
+        ),
+      );
+    });
   }
 }
