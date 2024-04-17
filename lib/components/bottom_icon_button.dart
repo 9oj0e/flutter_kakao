f273e0eb6c8911c3f0695bbeafdbf4a26c4c11ec
David Jo <war2290@gmail.com>
2024-04-17 14:26:00 +0900

    step04 : init

---
+++ lib/components/bottom_icon_button.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+class BottomIconButton extends StatelessWidget {
+  final IconData icon;
+  final String text;
+
+  const BottomIconButton({
+    Key? key,
+    required this.icon,
+    required this.text,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Column();
+  }
+}
---
+++ lib/pages/profile_screen.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+import '../models/user.dart';
+
+class ProfileScreen extends StatelessWidget {
+  final User user;
+
+  const ProfileScreen({Key? key, required this.user}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
