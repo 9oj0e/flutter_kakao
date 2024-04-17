32334622ec43bfc41ab2cea64dd857a19685bead
David Jo <war2290@gmail.com>
2024-04-17 15:40:30 +0900

    step06 : init

---
+++ lib/components/chat_icon_button.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class ChatIconButton extends StatelessWidget {
+  const ChatIconButton({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return SizedBox();
+  }
+}
---
+++ lib/components/other_chat.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class OtherChat extends StatelessWidget {
+  const OtherChat({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      children: [],
+    );
+  }
+}
