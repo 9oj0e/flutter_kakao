fa68b4a741b073cee0acd97299a17079f3d33ca2
David Jo <war2290@gmail.com>
2024-04-17 12:17:12 +0900

    step02 : main_page

--- lib/pages/main_page.dart
+++ lib/pages/main_page.dart
@@ -1,18 +1,54 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_kakao/pages/chat_page.dart';
+import 'package:flutter_kakao/pages/friend_page.dart';
+import 'package:flutter_kakao/pages/more_page.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 
 class MainPage extends StatefulWidget {
-  const MainPage({super.key});
-
   @override
   State<MainPage> createState() => _MainPageState();
 }
 
 class _MainPageState extends State<MainPage> {
+  int _selectedIndex = 0;
+
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      body: Center(
-        child: Text("Main Page"),
+    return Scaffold(
+      body: IndexedStack(
+        index: _selectedIndex,
+        children: [
+          FriendPage(),
+          ChatPage(),
+          MorePage(),
+        ],
+      ),
+      bottomNavigationBar: BottomNavigationBar(
+        showSelectedLabels: false,
+        showUnselectedLabels: false,
+        currentIndex: _selectedIndex,
+        backgroundColor: Colors.grey[100],
+        selectedItemColor: Colors.black,
+        unselectedItemColor: Colors.black54,
+        onTap: (index) {
+          setState(() {
+            _selectedIndex = index;
+          });
+        },
+        items: [
+          BottomNavigationBarItem(
+            icon: Icon(FontAwesomeIcons.user),
+            label: "user",
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(FontAwesomeIcons.comment),
+            label: "comment",
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(FontAwesomeIcons.ellipsisH),
+            label: "more",
+          ),
+        ],
       ),
     );
   }
